fd0955d42416c61ce51e2740874583e013cdf713
Matt Searles <matt@searles.com>
2020-02-28 21:07:14 +0000

    Adding aliases for git and macos defaults

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,15 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
